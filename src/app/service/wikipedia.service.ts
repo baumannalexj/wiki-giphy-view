763d77643c896e2c86d2fc93fa5dfb780f4bfc3b
baumannalexj <baumannalexj@gmail.com>
2018-10-28 18:43:16 -0500

    finished wiki search and view

--- src/app/service/wikipedia.service.ts
+++ src/app/service/wikipedia.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WikipediaService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  searchWikipedia(queryText: string) {
+    var queryUrl =
+      "https://en.wikipedia.org/w/api.php" +
+      "?action=query" +
+      "&format=json" +
+      "&list=search" +
+      "&origin=*" +
+      `&srsearch=${queryText}`;
+
+
+
+    let options = {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    };
+
+    return this.httpClient.get(queryUrl, options)
+  }
+
 }
