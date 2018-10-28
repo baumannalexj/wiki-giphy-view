e29c18ce6bfc0952e171c08b62b63de9a952ad5e
baumannalexj <baumannalexj@gmail.com>
2018-10-28 16:52:52 -0500

    gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const environment ={
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyDwKfGlNU1cNI_xpD2XyXAysh4FJbTQSEs",
+    authDomain: "csc-453-web-applications.firebaseapp.com",
+    databaseURL: "https://csc-453-web-applications.firebaseio.com",
+    projectId: "csc-453-web-applications",
+    storageBucket: "csc-453-web-applications.appspot.com",
+    messagingSenderId: "1084730653876"
+  },
+
+  giphyConfig : {
+    apiKey: "jdozUDLmhHErdgCq5vWPmxQWmlxl9Kc0",
+    baseUrl: "https://api.giphy.com/v1/gifs/search"
+  }
+
+};
