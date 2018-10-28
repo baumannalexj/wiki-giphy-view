183577310a4d88607bb69abfa4f8230737c39f4f
baumannalexj <baumannalexj@gmail.com>
2018-10-28 14:25:21 -0500

    routed 3 compoenents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HistoryComponent } from './component/history/history.component';
+import { RoutingModule} from "./module/routing/routing.module";
+import { WikipediaComponent } from './componenet/wikipedia/wikipedia.component';
+import { GiphyComponent } from './componenet/giphy/giphy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryComponent,
+    WikipediaComponent,
+    GiphyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
